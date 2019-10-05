412be57ba4c2153ce59f92b244bf844754dd16ea
Brian Portland <bponline@comcast.net>
2019-10-05 00:54:07 -0500

    Final submission for grade

--- app.py
+++ app.py
@@ -8,44 +8,54 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Book
+from models import Book, User, Room
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("welcome.html")
+    # return "Welcome to Brian Portland's page!\nI apologize - it's very barebones right now. But here's what you can do:\nGo to https://portland-spike.herokuapp.com/add/room to try adding a room to the database\nGo to https://portland-spike.herokuapp.com/getallrooms to see all currently existing rooms\nGo to https://portland-spike.herokuapp.com/get/room/ followed by an id to see the room with that id\nGo to https://portland-spike.herokuapp.com/remove/room/ followed by an id to remove a room with that id\n"
 
 @app.route("/add")
-def add_book():
-    name=request.args.get('name')
-    author=request.args.get('author')
-    published=request.args.get('published')
+def add_room():
+    lister = request.args.get('lister')
+    roommateGender = request.args.get('roommateGender')
+    numAvailable = request.args.get('numAvailable')
+    pets = request.args.get('pets')
+    shared = request.args.get('shared')
+    rent = request.args.get('rent')
+    ensuite = request.args.get('ensuite')
     try:
-        book=Book(
-            name=name,
-            author=author,
-            published=published
+        room=Room(
+            lister = lister,
+            roommateGender = roommateGender,
+            numAvailable = numAvailable,
+            pets = pets,
+            shared = shared,
+            rent = rent,
+            ensuite = ensuite
         )
-        db.session.add(book)
+        db.session.add(room)
         db.session.commit()
-        return "Book added. book id={}".format(book.id)
+        return "Room added. Room id={}".format(room.id)
     except Exception as e:
 	    return(str(e))
 
-@app.route("/getall")
-def get_all():
+@app.route("/getallrooms")
+def get_all_rooms():
     try:
-        books=Book.query.all()
-        return  jsonify([e.serialize() for e in books])
+        rooms=Room.query.all()
+        return  jsonify([e.serialize() for e in rooms])
     except Exception as e:
 	    return(str(e))
 
-@app.route("/get/<id_>")
-def get_by_id(id_):
+@app.route("/get/room/<id_>")
+def get_room_by_id(id_):
     try:
-        book=Book.query.filter_by(id=id_).first()
-        return jsonify(book.serialize())
+        room=Room.query.filter_by(id=id_).first()
+        return jsonify(room.serialize())
     except Exception as e:
 	    return(str(e))
+
 
 @app.route("/add/form",methods=['GET', 'POST'])
 def add_book_form():
@@ -65,6 +75,33 @@ def add_book_form():
         except Exception as e:
             return(str(e))
     return render_template("getdata.html")
+
+@app.route("/add/room",methods=['GET', 'POST'])
+def add_room_form():
+    if request.method == 'POST':
+        lister = request.form.get('lister')
+        roommateGender = request.form.get('roommateGender')
+        numAvailable = request.form.get('numAvailable')
+        pets = request.form.get('pets')
+        shared = request.form.get('shared')
+        rent = request.form.get('rent')
+        ensuite = request.form.get('ensuite')
+        try:
+            room=Room(
+                lister = lister,
+                roommateGender = roommateGender,
+                numAvailable = numAvailable,
+                pets = pets,
+                shared = shared,
+                rent = rent,
+                ensuite = ensuite
+            )
+            db.session.add(room)
+            db.session.commit()
+            return "Room added. Room id={}".format(room.id)
+        except Exception as e:
+            return(str(e))
+    return render_template("listroom.html")
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -23,3 +23,88 @@ class Book(db.Model):
             'author': self.author,
             'published':self.published
         }
+    
+class User(db.Model):
+    __tablename__ = 'users'
+
+    name = db.Column(db.String())
+    username = db.Column(db.String(), primary_key=True)
+    password = db.Column(db.String())
+    gender = db.Column(db.String())
+    listed = db.Column(db.ARRAY(db.Integer))
+    roommateGender = db.Column(db.String())
+    pets = db.Column(db.String())
+    shared = db.Column(db.String())
+    rentMin = db.Column(db.Float)
+    rentMax = db.Column(db.Float)
+    ensuite = db.Column(db.String())
+    favorites = db.Column(db.ARRAY(db.Integer))
+
+    def __init__(name, username, password, gender, listed, roommateGender, pets, shared, rentMin, rentMax, ensuite, favorites):
+        self.name = name
+        self.username = username
+        self.password = password
+        self.gender = gender
+        self.listed = listed
+        self.roommateGender = roommateGender
+        self.pets = pets
+        self.shared = shared
+        self.rentMin = rentMin
+        self.rentMax = rentMax
+        self.ensuite = ensuite
+        self.favorites = favorites
+
+    def __repr__(self):
+        return '<username {}>'.format(self.username)
+    
+    def serialize(self):
+        return {
+            'name': self.name,
+            'username': self.username,
+            'password': self.password,
+            'gender': self.gender,
+            'listed': self.listed,
+            'roommateGender': self.roommateGender,
+            'pets': self.pets,
+            'shared': self.shared,
+            'rentMin': self.rentMin,
+            'rentMax': self.rentMax,
+            'ensuite': self.ensuite,
+            'favorites': self.favorites
+        }
+
+class Room(db.Model):
+    __tablename__ = 'rooms'
+
+    id = db.Column(db.Integer, primary_key=True)
+    lister = db.Column(db.String())
+    roommateGender = db.Column(db.String())
+    numAvailable = db.Column(db.Integer)
+    pets = db.Column(db.String())
+    shared = db.Column(db.String())
+    rent = db.Column(db.Float)
+    ensuite = db.Column(db.String())
+
+    def __init__(self, lister, roommateGender, numAvailable, pets, shared, rent, ensuite):
+        self.lister = lister
+        self.roommateGender = roommateGender
+        self.numAvailable = numAvailable
+        self.pets = pets
+        self.shared = shared
+        self.rent = rent
+        self.ensuite = ensuite
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+    
+    def serialize(self):
+        return {
+            'id': self.id,
+            'lister': self.lister,
+            'roommateGender': self.roommateGender,
+            'numAvailable': self.numAvailable,
+            'pets': self.pets,
+            'shared': self.shared,
+            'rent': self.rent,
+            'ensuite': self.ensuite
+        }
